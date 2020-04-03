9a160528d9b64d861c6b03bcc5a37dedaa101ec5
Brian Mulhall <brian.e.mulhall@gmail.com>
2020-04-02 22:59:57 -0500

    Step 2.4: Create App component

---
+++ imports/ui/App.svelte
@@ -0,0 +1,28 @@
+<script>
+  import Task from './Task.svelte';
+  
+  function getTasks() {
+      return [
+        { _id: 1, text: "This is task 1" },
+        { _id: 2, text: "This is task 2" },
+        { _id: 3, text: "This is task 3" }
+      ];
+    }
+
+</script>
+ 
+
+<div class="container">
+  <header>
+    <h1>Todo List</h1>
+  </header>
+  <ul>
+  {#each getTasks() as task}
+    <Task
+      key={task._id}
+      task={task}
+    />
+  {/each}
+  </ul>
+</div>
+
