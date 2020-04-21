0e98d357dd4e2b7be2cd49d9c02300d5db29f099
Brian Mulhall <brian.e.mulhall@gmail.com>
2020-04-20 20:01:19 -0500

    Step 5.1: Update Task component to add features

--- imports/ui/Task.svelte
+++ imports/ui/Task.svelte
@@ -1,6 +1,33 @@
 <script>
+  import { Tasks } from "../api/tasks.js";
+
   export let key;
   export let task;
+
+  function toggleChecked() {
+      // Set the checked property to the opposite of its current value
+      Tasks.update(task._id, {
+        $set: { checked: !task.checked }
+      });
+  };
+
+  function deleteThisTask() {
+      Tasks.remove(task._id);
+  };
+
 </script>
 
-<li> { task.text }</li>
+<li class:checked="{task.checked}">
+  <button class="delete" on:click={deleteThisTask}>
+    &times;
+  </button>
+
+  <input
+    type="checkbox"
+    readonly
+    checked={!!task.checked}    
+    on:click={toggleChecked}
+  />
+
+  <span class="text">{ task.text }</span>
+</li>
